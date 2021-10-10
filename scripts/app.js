bb56e6861e046f96eae5473f8ac129fe962124ce
Menna Rashad <mennarashad301@gmail.com>
2021-10-10 21:03:18 +0200

    desktop styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,23 @@
-// if the custom tip input field is selected, the other radio inputs are unchecked
+// Tip calculation
+/*
+    ---------- Tip Calculations ------------
+    inputs: bill, ratio, numOfPeople
+    output: tip amount devided by one person
+
+    tip = bill * ratio / 100;
+    tip = 142.55 * 15 / 100
+    tip = 21.3825 total 
+    then tip per 1 person = tip / numOfPeople
+                          = 21.3825 / 5
+                          = 4.2765
+*/
+const bill = document.getElementById("bill");
+const ckeckedTipRadio = document.querySelector("input[type=radio]:checked");
 const tipRatioInput = document.getElementById("tipInput");
+const num = document.getElementById("num");
+const reset = document.getElementById("reset");
+
+// if the custom tip input field is selected, the other radio inputs are unchecked
 const radioElements = Array.from(
   document.querySelectorAll("input[type=radio]")
 );
