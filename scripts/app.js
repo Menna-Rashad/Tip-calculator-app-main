8791e82bca02a8106085927c8450638720ad74e5
Menna Rashad <mennarashad301@gmail.com>
2021-11-09 00:02:44 +0200

    tip calculations done

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 // Tip calculation
 const bill = document.getElementById("bill");
-const ckeckedTipRadio = document.querySelector("input[type=radio]:checked");
 const tipRatioInput = document.getElementById("tipInput");
 const numOfPeople = document.getElementById("num");
 const reset = document.getElementById("reset");
@@ -8,7 +7,7 @@ const form = document.getElementById("myForm");
 /*
     ---------- Tip Calculations ------------
     inputs: bill, ratio, numOfPeople
-    output: tip amount devided by one person
+    output: tip amount and (bill + tip) per person
 
     tip = bill * ratio / 100;
     tip = 142.55 * 15 / 100
@@ -16,7 +15,18 @@ const form = document.getElementById("myForm");
     then tip per 1 person = tip / numOfPeople
                           = 21.3825 / 5
                           = 4.2765
+
+    bill + tip = (142.55/4)+ 4.2765 =  whatever
 */
+function whichRatio(radio, text) {
+  if (radio) {
+    return Number(radio.value);
+  } else if (text) {
+    return Number(text.value);
+  } else {
+    return 0;
+  }
+}
 
 form.addEventListener("change", (e) => {
   reset.disabled = false;
@@ -28,8 +38,21 @@ form.addEventListener("submit", (e) => {
     console.log("can't be zero");
     const msg = document.querySelector("#errMsg");
     msg.style.display = "inline";
-    console.log(msg.style.display);
-    // numOfPeople.style.
+    //console.log(numOfPeople.style); (to display style properties' names)
+    numOfPeople.style.outlineColor = "red";
+  } else {
+    const billValue = Number(bill.value);
+    // maybe a function here that returns either tip ratio?
+    const ratio = whichRatio(
+      document.querySelector("input[type=radio]:checked"),
+      tipRatioInput
+    );
+    const people = Number(numOfPeople.value);
+
+    const tipAmount = document.getElementById("tipAmount");
+    tipAmount.textContent = `$${(billValue * ratio) / 100}`;
+    const total = document.getElementById("total");
+    total.textContent = `$${billValue / people + (billValue * ratio) / 100}`;
   }
 });
 
@@ -38,7 +61,7 @@ const radioElements = Array.from(
   document.querySelectorAll("input[type=radio]")
 );
 
-// focusin bubbles but focus doesn't
+// "focusin" bubbles but "focus" doesn't
 tipRatioInput.addEventListener("focus", () => {
   if (document.querySelector("input[type=radio]:checked")) {
     const checkedElement = document.querySelector("input[type=radio]:checked");
@@ -52,4 +75,19 @@ radioElements.forEach((ele) => {
   ele.addEventListener("click", () => {
     tipRatioInput.value = "";
   });
+});
+
+// Reset Button
+reset.addEventListener("click", (e) => {
+  bill.value = "";
+  numOfPeople.value = "";
+  tipRatioInput.value = "";
+  if (document.querySelector("input[type=radio]:checked")) {
+    const ckeckedTipRadio = document.querySelector("input[type=radio]:checked");
+    ckeckedTipRadio.checked = false;
+  }
+  const tipAmount = document.getElementById("tipAmount");
+  tipAmount.textContent = `$0.00`;
+  const total = document.getElementById("total");
+  total.textContent = `$0.00`;
 });
